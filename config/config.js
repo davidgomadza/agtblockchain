7dfdd43a50160f426c04d6fea11dcd20e02ab0f1
david gomadza <49430339+davidgomadza@users.noreply.github.com>
2025-11-03 18:32:07 +0000

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    blockchain: {
+        difficulty: 4,
+        miningReward: 100,
+        blockTime: 10000,
+        tokenDecimals: 18
+    },
+    network: {
+        port: 3000,
+        peers: [],
+        maxPeers: 10
+    },
+    tokens: {
+        genesisAllocation: {
+            "AGT": "37867890284",
+            "S96t'": "37867890284",
+            "BTCYT": "37867890284", 
+            "BTC": "37867890284",
+            "GTPS": "37867890284",
+            "AGOLD": "37867890284"
+        }
+    }
+};
